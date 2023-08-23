43d1017920775a8c7058ecec690f98a9f6e35454
akchindo <abdullahichindo16@gmail.com>
2023-08-23 22:00:24 +0100

    final fixes

---
+++ frontend/src/components/Meta.jsx
@@ -0,0 +1,19 @@
+import { Helmet } from 'react-helmet-async';
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: 'Welcome To Unishop',
+  description: 'We sell the best products for good prices',
+  keywords: 'household, books and pharmaceuticals',
+};
+
+export default Meta;
